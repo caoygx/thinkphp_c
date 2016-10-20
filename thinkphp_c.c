19e8beb9411458330ace3bf52e041e224ceb5fdc
root <root@localhost.localdomain>
2016-10-20 16:14:49 +0800

    init

--- thinkphp_c.c
+++ thinkphp_c.c
@@ -24,7 +24,13 @@
 
 #include "php.h"
 #include "php_ini.h"
+#include "main/SAPI.h"
+
 #include "ext/standard/info.h"
+#include "ext/standard/php_string.h"
+#include "ext/standard/php_var.h"
+#include "ext/standard/php_array.h"
+#include "Zend/zend_globals_macros.h"
 #include "php_thinkphp_c.h"
 
 /* If you declare any globals in php_thinkphp_c.h uncomment this:
@@ -33,14 +39,15 @@ ZEND_DECLARE_MODULE_GLOBALS(thinkphp_c)
 
 /* True global resources - no need for thread safety here */
 static int le_thinkphp_c;
+static zval *config =NULL;
 
 /* {{{ thinkphp_c_functions[]
  *
  * Every user visible function must have an entry in thinkphp_c_functions[].
  */
 const zend_function_entry thinkphp_c_functions[] = {
-	PHP_FE(confirm_thinkphp_c_compiled,	NULL)		/* For testing, remove later. */
-	PHP_FE_END	/* Must be the last line in thinkphp_c_functions[] */
+    PHP_FE(confirm_thinkphp_c_compiled, NULL)       /* For testing, remove later. */
+    PHP_FE_END  /* Must be the last line in thinkphp_c_functions[] */
 };
 /* }}} */
 
@@ -48,19 +55,19 @@ const zend_function_entry thinkphp_c_functions[] = {
  */
 zend_module_entry thinkphp_c_module_entry = {
 #if ZEND_MODULE_API_NO >= 20010901
-	STANDARD_MODULE_HEADER,
+    STANDARD_MODULE_HEADER,
 #endif
-	"thinkphp_c",
-	thinkphp_c_functions,
-	PHP_MINIT(thinkphp_c),
-	PHP_MSHUTDOWN(thinkphp_c),
-	PHP_RINIT(thinkphp_c),		/* Replace with NULL if there's nothing to do at request start */
-	PHP_RSHUTDOWN(thinkphp_c),	/* Replace with NULL if there's nothing to do at request end */
-	PHP_MINFO(thinkphp_c),
+    "thinkphp_c",
+    thinkphp_c_functions,
+    PHP_MINIT(thinkphp_c),
+    PHP_MSHUTDOWN(thinkphp_c),
+    PHP_RINIT(thinkphp_c),      /* Replace with NULL if there's nothing to do at request start */
+    PHP_RSHUTDOWN(thinkphp_c),  /* Replace with NULL if there's nothing to do at request end */
+    PHP_MINFO(thinkphp_c),
 #if ZEND_MODULE_API_NO >= 20010901
-	PHP_THINKPHP_C_VERSION,
+    PHP_THINKPHP_C_VERSION,
 #endif
-	STANDARD_MODULE_PROPERTIES
+    STANDARD_MODULE_PROPERTIES
 };
 /* }}} */
 
@@ -83,8 +90,8 @@ PHP_INI_END()
 /* Uncomment this function if you have INI entries
 static void php_thinkphp_c_init_globals(zend_thinkphp_c_globals *thinkphp_c_globals)
 {
-	thinkphp_c_globals->global_value = 0;
-	thinkphp_c_globals->global_string = NULL;
+    thinkphp_c_globals->global_value = 0;
+    thinkphp_c_globals->global_string = NULL;
 }
 */
 /* }}} */
@@ -93,10 +100,10 @@ static void php_thinkphp_c_init_globals(zend_thinkphp_c_globals *thinkphp_c_glob
  */
 PHP_MINIT_FUNCTION(thinkphp_c)
 {
-	/* If you have INI entries, uncomment these lines 
-	REGISTER_INI_ENTRIES();
-	*/
-	return SUCCESS;
+    /* If you have INI entries, uncomment these lines 
+    REGISTER_INI_ENTRIES();
+    */
+    return SUCCESS;
 }
 /* }}} */
 
@@ -104,10 +111,10 @@ PHP_MINIT_FUNCTION(thinkphp_c)
  */
 PHP_MSHUTDOWN_FUNCTION(thinkphp_c)
 {
-	/* uncomment this line if you have INI entries
-	UNREGISTER_INI_ENTRIES();
-	*/
-	return SUCCESS;
+    /* uncomment this line if you have INI entries
+    UNREGISTER_INI_ENTRIES();
+    */
+    return SUCCESS;
 }
 /* }}} */
 
@@ -116,7 +123,9 @@ PHP_MSHUTDOWN_FUNCTION(thinkphp_c)
  */
 PHP_RINIT_FUNCTION(thinkphp_c)
 {
-	return SUCCESS;
+        MAKE_STD_ZVAL(config);
+        array_init(config);
+    return SUCCESS;
 }
 /* }}} */
 
@@ -125,7 +134,8 @@ PHP_RINIT_FUNCTION(thinkphp_c)
  */
 PHP_RSHUTDOWN_FUNCTION(thinkphp_c)
 {
-	return SUCCESS;
+    zval_ptr_dtor(&config);
+    return SUCCESS;
 }
 /* }}} */
 
@@ -133,13 +143,13 @@ PHP_RSHUTDOWN_FUNCTION(thinkphp_c)
  */
 PHP_MINFO_FUNCTION(thinkphp_c)
 {
-	php_info_print_table_start();
-	php_info_print_table_header(2, "thinkphp_c support", "enabled");
-	php_info_print_table_end();
+    php_info_print_table_start();
+    php_info_print_table_header(2, "thinkphp_c support", "enabled");
+    php_info_print_table_end();
 
-	/* Remove comments if you have entries in php.ini
-	DISPLAY_INI_ENTRIES();
-	*/
+    /* Remove comments if you have entries in php.ini
+    DISPLAY_INI_ENTRIES();
+    */
 }
 /* }}} */
 
@@ -151,19 +161,224 @@ PHP_MINFO_FUNCTION(thinkphp_c)
 /* Every user-visible function in PHP should document itself in the source */
 /* {{{ proto string confirm_thinkphp_c_compiled(string arg)
    Return a string to confirm that the module is compiled in */
-PHP_FUNCTION(confirm_thinkphp_c_compiled)
+PHP_FUNCTION(C)
 {
-	char *arg = NULL;
-	int arg_len, len;
-	char *strg;
+    zval *key=NULL;
+    zval *value=NULL;
+    zval *default_value=NULL;
 
-	if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC, "s", &arg, &arg_len) == FAILURE) {
-		return;
-	}
 
-	len = spprintf(&strg, 0, "Congratulations! You have successfully modified ext/%.78s/config.m4. Module %.78s is now compiled into PHP.", "thinkphp_c", arg);
-	RETURN_STRINGL(strg, len, 0);
-}
+    if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC, "|zzz", &key, &value, &default_value) == FAILURE) {
+        php_printf("param error");
+        RETURN_NULL();  
+    }
+    
+    
+
+    if(key== NULL || Z_TYPE_P(key) == IS_NULL){
+//      php_printf("key is null,return all \n");    
+
+//      ZVAL_COPY_VALUE(return_value,config);
+        RETURN_ZVAL(config,1,0);
+//      return;
+    }
+
+    if(Z_TYPE_P(key) == IS_STRING ){
+
+        char *name;
+        //strcpy(name,Z_STRVAL_P(key));
+        name = Z_STRVAL_P(key);
+//      name = (char *)emalloc(Z_STRLEN_P(key)+1);
+//      ZVAL_COPY_VALUE(key_copy,key);
+        //strcpy(name,Z_STRVAL_P(key_copy));
+        //memcpy(name,Z_STRVAL_P(key),Z_STRLEN_P(key)+1);
+//
+//      zval *key_copy;
+//      MAKE_STD_ZVAL(key_copy);
+//      ZVAL_COPY_VALUE(key_copy,key);
+//      php_var_dump(&config,1);    
+//                  zval_ptr_dtor(&key);    
+//              php_var_dump(&config,1);
+//      name = php_strtoupper(Z_STRVAL_P(key),Z_STRLEN_P(key));
+//              php_var_dump(&config,1);
+//php_var_dump(&key_copy,1);return;
+        //name = php_strtoupper(name,strlen(name));
+//php_printf("len:%d\n",sizeof(name));
+//php_printf("len_zval:%d\n",Z_STRLEN_P(key));
+//              php_var_dump(&config,1);    
+        if (!strstr(name, ".")) {
+//          php_printf("key:%s",name); 
+//          php_printf("\n"); 
+
+            if(value == NULL || Z_TYPE_P(value) == IS_NULL){//getc
+ //               php_printf("get start:");
+//                php_var_dump(&config,1);
+
+                zval **ppdata;
+                if(zend_hash_find(Z_ARRVAL_P(config),name,strlen(name)+1,(void **)&ppdata) != FAILURE)
+                {
+                
+//                    ZVAL_COPY_VALUE
+                    RETURN_ZVAL(*ppdata,1,0);
+            //      php_var_dump(ppdata,1);         
+                    //ZVAL_COPY_VALUE(return_value,*ppdata);
+                    //efree(*ppdata);
+//                  php_printf("find"); 
+//                  zval_ptr_dtor(ppdata);  
+                    //zval_dtor(*ppdata);
+        //          efree(name);
+                //  return;
+                }
+                if(default_value != NULL && Z_TYPE_P(default_value) != IS_NULL  ){
+//                  php_printf(Z_STRVAL_P(key)); 
+//                  php_printf(":"); 
+//                  php_printf("no find\n"); 
+                    ZVAL_COPY_VALUE(return_value,default_value);
+                    //                 php_var_dump(&default_value,1);
+        //          zval_dtor(default_value);
+                    return;
+                }
+                return ;
+            }else{ //set
+
+//              zval *tmp2;
+//              MAKE_STD_ZVAL(tmp2);
+                //ALLOC_ZVAL(tmp2);
+                //INIT_PZVAL_COPY(tmp2,value);
+  //              tmp2 = value;
+                //Z_ADDREF_P(value);
+    //            zval_copy_ctor(tmp2);
+    Z_ADDREF_P(value);
+ //   php_printf(Z_STRVAL_P(value));
+                zend_hash_update(Z_ARRVAL_P(config), name, strlen(name)+1, &value, sizeof(zval *), NULL);
+       //         Z_DELREF_P(value);       
+        // Z_ADDREF_P(value);
+        //      efree(name);
+//              zval_ptr_dtor(&tmp2);
+  //              php_printf("set end:");
+//                php_var_dump(&config,1);
+    RETURN_TRUE;
+            }
+            return;
+        }
+        //二维数据设置和获取
+        char *firstKeyPointer= NULL;
+        char *firstKey = NULL;
+
+        char *secondKeyPointer= NULL;
+        char *secondKey= NULL;
+        //char *nameCopy = NULL;
+        char *nameCopy = (char *)malloc(strlen(name)+1);
+        strcpy(nameCopy,name);
+        php_printf("copy=%s\n",nameCopy);
+        firstKey= strtok(nameCopy,".");
+        secondKey= strtok(NULL,".");
+
+        if(value == NULL || Z_TYPE_P(value) == IS_NULL){
+            zval **ppzval;
+            if(zend_hash_find(Z_ARRVAL_P(config),firstKey,strlen(firstKey)+1,(void **)&ppzval) != FAILURE){
+                //                    php_var_dump(ppzval,1);
+                //php_var_dump(ppzval,1);
+                zval **secondValue=NULL;
+                if(zend_hash_find(Z_ARRVAL_P(*ppzval),secondKey,strlen(secondKey)+1,(void **)&secondValue) != FAILURE){
+
+                    RETURN_ZVAL(*secondValue,1,0);
+                }
+        //      zval_ptr_dtor(ppzval);  
+            }
+            if(default_value != NULL && Z_TYPE_P(default_value) != IS_NULL  ){
+                //php_printf(Z_STRVAL_P(key)); 
+//              php_printf("."); 
+//              php_printf("no find\n"); 
+                ZVAL_COPY_VALUE(return_value,default_value);
+                //                 php_var_dump(&default_value,1);
+                return;
+            }else{
+                RETURN_NULL();
+            }
+
+        }else{
+
+            zval *tmp;
+            MAKE_STD_ZVAL(tmp);
+            zval **ppzval;
+
+            if(zend_hash_find(Z_ARRVAL_P(config),firstKey,strlen(firstKey)+1,(void **)&ppzval) != FAILURE){
+                ZVAL_COPY_VALUE(tmp,value);
+                zend_hash_update(Z_ARRVAL_PP(ppzval),secondKey,strlen(secondKey)+1,(void **)&tmp,sizeof(zval *),NULL);
+//              php_printf("%s\n","find"); 
+        //      zval_ptr_dtor(ppzval);  
+            }
+            else{
+                zval *firstArray;
+                MAKE_STD_ZVAL(firstArray);
+                array_init(firstArray);
+                //add_assoc_string(firstArray,secondKey,"api",1);
+                ZVAL_COPY_VALUE(tmp,value);
+                add_assoc_zval(firstArray,secondKey,tmp);
+                add_assoc_zval(config,firstKey,firstArray);
+            }
+        }
+    }
+    if(Z_TYPE_P(key) == IS_ARRAY){
+
+        zend_fcall_info fci;
+        zend_fcall_info_cache fcc;
+        zval *retval;
+        zval *handle = NULL;
+        zval function_name;
+        zval **argv[2];
+
+        ZVAL_STRING(&function_name, "array_change_key_case", 1);
+        MAKE_STD_ZVAL(handle);
+
+        zval *type; 
+        MAKE_STD_ZVAL(type);
+        ZVAL_LONG(type,1);
+        argv[0] = &key;
+        argv[1] = &type;
+        fci.size = sizeof(fci);
+        fci.function_table = EG(function_table);
+        fci.function_name = &function_name;
+        fci.symbol_table = NULL;
+        fci.object_ptr = NULL;
+        fci.retval_ptr_ptr = &retval;
+        fci.param_count = 2;
+        fci.params = argv;
+        fci.no_separation = 0;
+
+
+//php_var_dump(&key,1);
+        if (zend_call_function(&fci, NULL TSRMLS_CC) == FAILURE) {
+            php_printf("err");        
+        }
+        else {
+//php_var_dump(&retval,1);
+            php_array_merge(Z_ARRVAL_P(config),Z_ARRVAL_PP(&retval), 0 TSRMLS_CC);
+            //zval_ptr_dtor(&key);
+        }
+//      zval_ptr_dtor(&retval);
+        zval_ptr_dtor(&handle);
+        zval_ptr_dtor(&type);
+        zval_dtor(&function_name);
+        zval_ptr_dtor(*argv);
+        RETURN_TRUE;
+    }
+
+
+
+        RETURN_NULL();  
+
+
+
+
+
+
+
+    //len = spprintf(&strg, 0, "Congratulations! You have successfully modified ext/%.78s/config.m4. Module %.78s is now compiled into PHP.", "C", arg);
+    //RETURN_STRINGL(strg,  0);
+    }
+
 /* }}} */
 /* The previous line is meant for vim and emacs, so it can correctly fold and 
    unfold functions in source code. See the corresponding marks just before 
@@ -180,3 +395,4 @@ PHP_FUNCTION(confirm_thinkphp_c_compiled)
  * vim600: noet sw=4 ts=4 fdm=marker
  * vim<600: noet sw=4 ts=4
  */
+
